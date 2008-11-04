ba4cc7645e63fa3c0879ddc578fc79bb7b906da0
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2008-11-04 04:32:28 +0000

    Tagging Editra 0.3.90
    git-svn-id: http://svn.wxwidgets.org/svn/wx/wxPython@56676 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ 3rdParty/tags/Editra-0.3.90/scripts/i18n/gen_lang.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+##############################################################################
+# Generates po and mo files from the project source code
+# Author: Cody Precord
+# Copyright: Cody Precord <cprecord@editra.org>
+# License: wxWindows
+##############################################################################
+
+##############################################################################
+# Variables
+##############################################################################
+ARG=$1
+
+##############################################################################
+# Function: print_help
+# Purpose: Print the scripts usage help to the console
+##############################################################################
+print_help () {
+	echo
+	echo "Usage: $0 [-h|-mo|-po|-all]"
+	echo "    -h    Print this help message"
+	echo "    -mo   Generate mo files and install them in the locale directory"
+	echo "    -po   Generate new po files from the project source"
+    echo "    -all  Regenerate everything"
+	echo
+}
+
+##############################################################################
+# Function: gen_po
+# Purpose: Generate new po files from the source
+##############################################################################
+gen_po () {
+    ./mki18n.py -pv --domain=Editra
+    # Copy all .new files to override the originals
+    for fname in $(ls); do
+        if ! [ -z $(echo $fname | grep '.*\.new') ]; then
+            name=$(echo $fname | sed 's/.new//')
+            mv $fname $name
+        fi
+    done
+}
+
+##############################################################################
+# Function: make_mo
+# Purpose: Make mo files and place them in the appropriate locale directory
+##############################################################################
+make_mo () {
+    ./mki18n.py -mv --domain=Editra --moTarget=../../locale
+}
+
+
+##############################################################################
+# Main
+##############################################################################
+
+if [ "$ARG" = "-po" ]
+then
+    gen_po
+elif [ "$ARG" = "-mo" ]
+then
+    make_mo
+elif [ "$ARG" = "-all" ]
+then
+    gen_po
+    make_mo
+else
+    print_help
+fi    
