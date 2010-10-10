c684dfc702100de74b041dd884c3696e02d63205
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2010-10-10 21:28:56 +0000

    Tag Editra 0.5.86
    git-svn-id: http://svn.wxwidgets.org/svn/wx/wxPython@65792 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ 3rdParty/tags/Editra-0.5.86/src/ebmlib/__init__.py
@@ -0,0 +1,38 @@
+###############################################################################
+# Name: __init__.py                                                           #
+# Purpose: Editra Buisness Model Library                                      #
+# Author: Cody Precord <cprecord@editra.org>                                  #
+# Copyright: (c) 2009 Cody Precord <staff@editra.org>                         #
+# Licence: wxWindows Licence                                                  #
+###############################################################################
+
+"""
+Editra Buisness Model Library:
+
+"""
+
+__author__ = "Cody Precord <cprecord@editra.org>"
+__cvsid__ = "$Id$"
+__revision__ = "$Revision$"
+
+#-----------------------------------------------------------------------------#
+
+# Text Utils
+from searcheng import *
+from fchecker import *
+from fileutil import *
+from fileimpl import *
+from txtutil import *
+
+from backupmgr import *
+from calllock import *
+
+# Storage Classes
+from histcache import *
+from clipboard import *
+
+# Misc
+from miscutil import *
+from cmenumgr import *
+from osutil import *
+
