6a743fe8b0168acb88ffbc512049a4fd13a0535f
AG <AG@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2011-01-17 14:26:29 +0000

    (1) AuiNotebook:
    Applied a bunch of patches from Luke McCarthy:
    
    - Eliminated unnecessary refreshing in SetPageText(), SetPageBitmap() and SetPageTextColour(). This change made a massive difference to
    performance of our application when there are a reasonable number (e.g.
    30) of tabs or docks, because the AuiManager Update() was calling them
    every time (and thus redrawing) even when nothing had changed. For
    example, it could take several seconds to dock a window, and minutes to
    start up a session with LoadPerspective().
    
    - Handle rapid clicking of notebook buttons correctly. Before this change
    it would ignore every other click when clicking rapidly, so closing down
    many tabs was slow and frustrating.
    
    - Bug fix: _tab_offset could be invalid after removing a page.
    
    - When there are a lot of tabs, closing tabs did not shuffle tabs along to
    make use of the available space. This patch will make sure that all the
    available space in the tab control is used.
    
    - Focus the active page if the empty area of the tab control is clicked (this is useful when using multiple notebooks with AuiManager).
    
    - Check page index returned from GetPageIndex (has been known to return -1).
    
    - Possibly fixed ticket #12742 (drop menu crash).
    
    - Removed sizing cursor (fixes #12803).
    
    - Added external DnD event (fixes #12811).
    
    
    2) HyperTreeList:
    
    - Fixed infinite loop bug (index was never incremented).
    - Select all text in edit control when created.
    - Set column for EVT_TREE_END_LABEL_EDIT event.
    - If double clicked in same row but different column now closes any open edit first.
    - Wheel mouse now works in when not directly over tree item.
    - Clip the drawing of expand/collapse tree buttons.
    - Applied patches from Frank Niessink to fix #12848 and #12851.
    
    
    git-svn-id: http://svn.wxwidgets.org/svn/wx/wxPython@66700 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- 3rdParty/AGW/agw/hypertreelist.py
+++ 3rdParty/AGW/agw/hypertreelist.py
@@ -3,7 +3,7 @@
 # Inspired By And Heavily Based On wx.gizmos.TreeListCtrl.
 #
 # Andrea Gavana, @ 08 May 2006
-# Latest Revision: 28 Nov 2010, 16.00 GMT
+# Latest Revision: 12 Jan 2011, 18.00 GMT
 #
 #
 # TODO List
@@ -214,7 +214,7 @@ License And Version
 
 HyperTreeList is distributed under the wxPython license.
 
-Latest Revision: Andrea Gavana @ 28 Nov 2010, 16.00 GMT
+Latest Revision: Andrea Gavana @ 12 Jan 2011, 18.00 GMT
 
 Version 1.2
 
@@ -914,6 +914,7 @@ class TreeListHeaderWindow(wx.Window):
         while idx <= col:
             
             if not self.IsColumnShown(idx):
+                idx += 1
                 continue 
 
             column = self.GetColumn(idx)
@@ -3066,7 +3067,9 @@ class TreeListMainWindow(CustomTreeCtrl):
                         button[2].x = button[0].x + (self._btnWidth2+1)
                         button[2].y = y_mid
                     
+                    dc.SetClippingRegion(x_maincol + _MARGIN, y_top, clip_width, h)
                     dc.DrawPolygon(button)
+                    dc.DestroyClippingRegion()
 
                 else: # if (HasAGWFlag(wxTR_HAS_BUTTONS))
 
@@ -3294,6 +3297,7 @@ class TreeListMainWindow(CustomTreeCtrl):
         self._textCtrl = EditTextCtrl(self, -1, self._editItem, column,
                                       self, self._editItem.GetText(column),
                                       style=style|wx.TE_PROCESS_ENTER)
+        self._textCtrl.SelectAll()
         self._textCtrl.SetFocus()
 
 
@@ -3316,6 +3320,7 @@ class TreeListMainWindow(CustomTreeCtrl):
         le.SetItem(self._editItem)
         le.SetEventObject(self._owner)
         le.SetLabel(value)
+        le.SetInt(self._curColumn if self._curColumn >= 0 else 0)
         le._editCancelled = False
         self._owner.GetEventHandler().ProcessEvent(le)
 
@@ -3395,7 +3400,7 @@ class TreeListMainWindow(CustomTreeCtrl):
 
         if (event.LeftDown() or event.LeftUp() or event.RightDown() or \
             event.RightUp() or event.LeftDClick() or event.Dragging()):
-            if self._textCtrl != None and item != self._textCtrl.item():
+            if self._textCtrl != None and (item != self._textCtrl.item() or column != self._textCtrl.column()):
                 self._textCtrl.StopEditing()
 
         # We do not want a tooltip if we are dragging, or if the rename timer is running
@@ -3540,7 +3545,7 @@ class TreeListMainWindow(CustomTreeCtrl):
             self._dragCount = 0
 
         # we process only the messages which happen on tree items
-        if item == None or not self.IsItemEnabled(item):
+        if (item == None or not self.IsItemEnabled(item)) and not event.GetWheelRotation():
             self._owner.GetEventHandler().ProcessEvent(event)
             return
         
@@ -3584,7 +3589,7 @@ class TreeListMainWindow(CustomTreeCtrl):
             
             # determine the selection if not done by left down
             if not self._left_down_selection:
-                unselect_others = not ((event.ShiftDown() or event.ControlDown()) and self.HasAGWFlag(wx.TR_MULTIPLE))
+                unselect_others = not ((event.ShiftDown() or event.CmdDown()) and self.HasAGWFlag(wx.TR_MULTIPLE))
                 self.DoSelectItem(item, unselect_others, event.ShiftDown())
                 self.EnsureVisible (item)
                 self._current = self._key_current = item # make the new item the current item
@@ -3624,7 +3629,7 @@ class TreeListMainWindow(CustomTreeCtrl):
                 
             # determine the selection if the current item is not selected
             if not item.IsSelected():
-                unselect_others = not ((event.ShiftDown() or event.ControlDown()) and self.HasAGWFlag(wx.TR_MULTIPLE))
+                unselect_others = not ((event.ShiftDown() or event.CmdDown()) and self.HasAGWFlag(wx.TR_MULTIPLE))
                 self.DoSelectItem(item, unselect_others, event.ShiftDown())
                 self.EnsureVisible(item)
                 self._current = self._key_current = item # make the new item the current item
@@ -3953,7 +3958,7 @@ class TreeListMainWindow(CustomTreeCtrl):
             # next sibling
             item, cookie = self.GetNextChild(parent, cookie)
         
-        return width
+        return max(10, width) # Prevent zero column width
 
 
     def HideItem(self, item, hide=True):
