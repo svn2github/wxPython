aad71115e68b0d8325d618f952eda7842562c1d2
CHB <CHB@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2014-02-03 23:49:13 +0000

    Put in a check to prevent a crash if a ScaledBitmap was sized to less than one pixel.
    git-svn-id: http://svn.wxwidgets.org/svn/wx/wxPython@75781 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ 3rdParty/FloatCanvas/Demos/BitmapAutoZoom.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+"""
+This is a demo of putting two bitmaps on a Canvas, and auto-zooming them.
+
+This is here as much as a test as anything else, as it found a bug (now fixed) in the bitmap scaling code
+
+Example provided by: nouvellecollection@gmail.com
+
+"""
+
+import wx
+
+#### import local version:
+import sys
+sys.path.append("..")
+from floatcanvas import NavCanvas, FloatCanvas
+
+#from wx.lib.floatcanvas import FloatCanvas
+
+class MyPanel(wx.Panel):
+    def __init__(self, parent):
+        super(MyPanel, self).__init__(parent)
+        self.SetBackgroundColour(wx.WHITE)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        self.Canvas = FloatCanvas.FloatCanvas(self, BackgroundColor = "LIGHT GREY")
+        self.Canvas.MaxScale = 2        
+
+        sizer.Add(self.Canvas, 1, flag=wx.EXPAND)
+
+        bmp1 = wx.BitmapFromImage(wx.Image('bmp1.png', wx.BITMAP_TYPE_PNG))
+        bmp2 = wx.BitmapFromImage(wx.Image('bmp2.png', wx.BITMAP_TYPE_PNG))            
+        img1 = self.Canvas.AddScaledBitmap2(bmp1, (0, 0), Height = bmp1.GetHeight(), Position = 'tl')
+        img2 = self.Canvas.AddScaledBitmap2(bmp2, (100, -100), Height = bmp2.GetHeight(), Position = 'tl')
+        
+        self.Canvas.ZoomToBB()
+        #wx.CallLater(1, self.Canvas.ZoomToBB)
+
+        self.Bind(wx.EVT_SIZE, self.OnSize)
+
+        self.SetSizerAndFit(sizer)
+
+    def OnSize(self, evt):
+        wx.CallLater(1, self.Canvas.ZoomToBB)
+        evt.Skip()
+     
+class DemoApp(wx.App):
+        def __init__(self, *args, **kwargs):
+            wx.App.__init__(self, *args, **kwargs)
+            frame = wx.Frame(None)
+            MyPanel(frame)
+            frame.Show()
+            frame.Fit()
+
+app = DemoApp(False)
+app.MainLoop()
